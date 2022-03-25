948ed7a6c72d53ee410a0262d40fed246b956f75
Hajaniaina <piadmin@toamasina.local>
2022-03-25 16:28:42 +0300

    centered the login page

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ Amplify.configure(awsExports);
 
 export default function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1 className="">Hello {user.username}</h1>
-          <button className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded py-3 px-4 m-5" onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <div className="flex h-screen w-screen bg-gradient-to-b from-indigo-100 via-purple-100 to-pink-200">
+      <Authenticator className="shadow-lg">
+        {({ signOut, user }) => (
+          <main>
+            <h1 className="">Hello {user.username}</h1>
+            <button className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded py-3 px-4 m-5" onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </div>
   );
 }
